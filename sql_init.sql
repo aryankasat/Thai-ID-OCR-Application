dc7158a5ae89354b062cfc43225cc24c30d5e20e
aryankasat <20uec034@lnmiit.ac.in>
2023-12-25 21:27:07 +0530

    Creation of the MySQL Database

--- sql_init.sql
+++ sql_init.sql
@@ -3,11 +3,8 @@ CREATE DATABASE IF NOT EXISTS ocr;
 USE ocr;
 
 CREATE TABLE IF NOT EXISTS thai_id_cards (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
-    identification_number VARCHAR(20) NOT NULL,
-    date_of_issue DATE,
-    date_of_expiry DATE,
-    date_of_birth DATE
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+    status VARCHAR(10),
+    ocr_result JSON
 );
